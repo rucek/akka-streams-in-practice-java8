cc892784fc7c6482c0bc783a3e04daea073f97c1
Jacek Kunicki <jacek.kunicki@softwaremill.com>
2017-05-11 10:52:38 +0200

    File parser

--- src/main/java/org/kunicki/akka_streams_java8/importer/CsvImporter.java
+++ src/main/java/org/kunicki/akka_streams_java8/importer/CsvImporter.java
@@ -5,6 +5,7 @@ import akka.actor.ActorSystem;
 import akka.stream.javadsl.Flow;
 import akka.stream.javadsl.Framing;
 import akka.stream.javadsl.FramingTruncation;
+import akka.stream.javadsl.StreamConverters;
 import akka.util.ByteString;
 import com.typesafe.config.Config;
 import io.vavr.control.Try;
@@ -16,9 +17,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.nio.file.Paths;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
+import java.util.zip.GZIPInputStream;
 
 public class CsvImporter {
 
@@ -58,4 +61,15 @@ public class CsvImporter {
 
   private Flow<ByteString, ByteString, NotUsed> lineDelimiter =
       Framing.delimiter(ByteString.fromString("\n"), 128, FramingTruncation.ALLOW);
+
+  private Flow<File, Reading, NotUsed> parseFile() {
+    return Flow.of(File.class).flatMapConcat(file -> {
+      GZIPInputStream inputStream = new GZIPInputStream(new FileInputStream(file));
+      return StreamConverters.fromInputStream(() -> inputStream)
+          .via(lineDelimiter)
+          .drop(linesToSkip)
+          .map(ByteString::utf8String)
+          .mapAsync(nonIOParallelism, this::parseLine);
+    });
+  }
 }
